d62e3a4d3b54c675e547a436eed5a6588e3234d4
Ryan Wetzelberger <rwetzelberger@videologygroup.com>
2017-06-26 11:41:54 -0400

    adding setup script for iterm2.  Needs more work

---
+++ iterm2install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# This script will install iterm2 with Oh My Zsh set up git.
+# It pulls all variables from bash_profile, just in case one needs to use
+# built-in Terminal for MacOS.
+
+# install iterm2
+# still needs work
+itermzip = "iTerm2-3_0_15.zip"
+
+wget "https://iterm2.com/downloads/stable/$itermzip"
+unzip $itermzip
+rm $itermzip
+
+mv iTerm ~/Applications
+
+# set up Oh My Zsh
+# still needs work
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# git changes
+git config --global color.ui true
